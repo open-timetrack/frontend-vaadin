ba05946d43c26a19da0227f97a8dbf6ffcde624f
r-vanooyen <robin.vanooyen@gmail.com>
2024-01-22 08:14:15 +0100

    fix: script to start db

--- startDb.sh
+++ startDb.sh
@@ -1,2 +1,2 @@
 #!/bin/zsh
-docker run -d --mount type=bind,source=$(pwd)/db,target=/var/lib/postgresql/data -e POSTGRES_PASSWORD=root  postgres:15.3
+docker run -d --mount type=bind,source=$(pwd)/db,target=/var/lib/postgresql/data -e POSTGRES_PASSWORD=root --restart always -p 50432:5432 postgres:15.5
